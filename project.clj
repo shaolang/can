29398da41ef18ac72ed77746365f9760d46a9327
shaolang <shaolang@ymail.com>
2019-03-23 22:32:56 +0800

    Bump version up for development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject can "0.2.0"
+(defproject can "0.2.1-SNAPSHOT"
   :description "Permissions library for Clojure[Script]"
   :url "https://github.com/shaolang/can"
   :license {:name "EPL-2.0"
